c27442241688b85c34c3f44b3dbb40f02b9085d0
Adorfer <adorferen@gmail.com>
2022-07-27 01:50:05 +0200

    Update batman-adv-dkmsadd.sh

--- batman_install/batman-adv-dkmsadd.sh
+++ batman_install/batman-adv-dkmsadd.sh
@@ -3,6 +3,7 @@ batversion="2022.1"
 #export  batversion="2022.1"
 
 # prerequirements  (base batman include)
+apt-get update
 apt-get install -y build-essential
 apt-get install -y dkms
 apt-get install -y libnl-3-dev libnl-genl-3-dev
