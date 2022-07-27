ee39339c72c56563f63d17c8fdf1b474b074e5e9
Adorfer <adorferen@gmail.com>
2022-07-27 03:05:17 +0200

    holding batctl

--- batman_install/batman-adv-dkmsadd.sh
+++ batman_install/batman-adv-dkmsadd.sh
@@ -6,6 +6,7 @@ batversion="2022.1"
 apt-get update
 apt-get install -y build-essential pkg-config libnl-3-dev libnl-genl-3-dev
 apt-get install -y batctl bridge-utils
+apt-mark hold batctl
 apt-get install -y dkms
 grep -q -c ^batman-adv /etc/modules || printf "\nbatman-adv\n" >>/etc/modules
 
